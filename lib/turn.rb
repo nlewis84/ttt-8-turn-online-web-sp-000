dc1e3149d02d87a4ae9786c30f3da0899cf34e01
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-16 02:57:04 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -42,7 +42,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  input_to_index(input)
+  index = input_to_index(input)
   if valid_move?(board, index) === false
     turn(board)
   else
